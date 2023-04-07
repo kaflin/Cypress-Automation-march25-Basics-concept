44194c18f0408179862f3f8053439e6f28b8ec07
suraj_gupta <suraj520876@gmail.com>
2023-04-07 21:11:10 +0545

    Added in Alerts

---
+++ cypress/e2e/Alerts.cy.js
@@ -0,0 +1,10 @@
+describe('Alerts', () => {
+    // 1)Javascript Alert:It will have some text and an "OK" Button
+
+    //2) javascript Confirm Alert: It will have some text with 'OK' and 'Cancel' Buttons
+
+    //3) Javascript Prompt Alert: It will have some text with a text box for user input along with "Ok"
+
+    //4) Authenticated Alert
+
+})

89b871cd0235db34a1e02ce4e4f4ba876eefc163
Suraj Gupta <suraj.gupta@omnicommediagroup.com.np>
2023-05-13 09:35:51 +0545

    Screen and Videos capturing

--- cypress/e2e/CaptureScreenshotAndVideos.cy.js
+++ cypress/e2e/CaptureScreenshotAndVideos.cy.js
@@ -8,10 +8,10 @@ describe('mysuite',()=>{
         //Capture screenshot of specific element or logo element
         // cy.get("img[title='Your Store']").screenshot("logo");
 
-        //Automatically capture screenshot & videos  on failure - only when execute through CLI
+        //Automatically capture screenshot & videos  on failure - only when execute through CLI or command prompt or in terminal i.e, npx cypress run --spec cypress\e2e\CaptureScreenshotAndVideos.cy.js
         cy.get("li:nth-child(7) a:nth-child(1)").click();//cameras
         cy.get("div[id='content'] h2").should('have.text',"Tablets");//Expecting Cameras text but found Tablets text,Intentailloy to fail test to get ss and videos
-        
+
 
 
 

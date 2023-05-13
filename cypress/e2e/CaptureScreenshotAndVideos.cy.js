eab57e8de5cafae7369b0b068e28cb4178d68ecf
Suraj Gupta <suraj.gupta@omnicommediagroup.com.np>
2023-05-13 09:33:22 +0545

    Screen and Videos capturing

---
+++ cypress/e2e/CaptureScreenshotAndVideos.cy.js
@@ -0,0 +1,23 @@
+describe('mysuite',()=>{
+    it('Capture screenshots & videos',()=>{
+        cy.visit("https://demo.opencart.com/")
+
+        // cy.screenshot("homepage");//Name to that particular screenshot,Capturing entire page
+        // cy.wait(5000);
+
+        //Capture screenshot of specific element or logo element
+        // cy.get("img[title='Your Store']").screenshot("logo");
+
+        //Automatically capture screenshot & videos  on failure - only when execute through CLI
+        cy.get("li:nth-child(7) a:nth-child(1)").click();//cameras
+        cy.get("div[id='content'] h2").should('have.text',"Tablets");//Expecting Cameras text but found Tablets text,Intentailloy to fail test to get ss and videos
+        
+
+
+
+
+
+
+
+    })
+})
